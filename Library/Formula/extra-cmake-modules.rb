75b183aa45d67a3df55f8574c4f8dbb0f01f1c71
Harald Fernengel <harald.fernengel@nokia.com>
2014-02-02 21:41:26 +0100

    patch

--- Library/Formula/extra-cmake-modules.rb
+++ Library/Formula/extra-cmake-modules.rb
@@ -11,8 +11,27 @@ class ExtraCmakeModules < Formula
 
   depends_on "cmake" => :build
 
+  def patches
+    DATA
+  end
+
   def install
     system "cmake", ".", *std_cmake_args
     system "make", "install"
   end
 end
+
+__END__
+diff --git a/kde-modules/KDECompilerSettings.cmake b/kde-modules/KDECompilerSettings.cmake
+index 335e127..3b546c5 100644
+--- a/kde-modules/KDECompilerSettings.cmake
++++ b/kde-modules/KDECompilerSettings.cmake
+@@ -273,7 +273,7 @@ endfunction()
+ ############################################################
+ 
+ if ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "GNU" OR
+-        "${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang" OR
++        ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Clang" AND NOT APPLE) OR
+         ("${CMAKE_CXX_COMPILER_ID}" STREQUAL "Intel" AND NOT WIN32))
+     # Linker warnings should be treated as errors
+     set(CMAKE_SHARED_LINKER_FLAGS "-Wl,--fatal-warnings ${CMAKE_SHARED_LINKER_FLAGS}")
