3506d1c300e798ad257e724209245b001c33f81c
Harald Fernengel <harald.fernengel@nokia.com>
2014-02-02 21:38:45 +0100

    add kconfig

---
+++ Library/Formula/kconfig.rb
@@ -0,0 +1,24 @@
+require "formula"
+
+class Kconfig < Formula
+  homepage "http://www.kde.org/"
+  url "http://download.kde.org/unstable/frameworks/4.95.0/kconfig-4.95.0.tar.xz"
+  sha1 "0d896407a8f5d8a14df2456a8681c39b583bf9e5"
+
+  keg_only "Only required for building KDE applications"
+
+  head 'git://anongit.kde.org/kconfig.git'
+
+  depends_on "cmake" => :build
+  depends_on "extra-cmake-modules" => :build
+  depends_on "qt5" => :build
+
+  def install
+    args = std_cmake_args
+    args << "-DCMAKE_PREFIX_PATH=\"#{Formula.factory('qt5').opt_prefix};#{Formula.factory('extra-cmake-modules').opt_prefix}\""
+    args << "-DCMAKE_PREFIX_PATH="
+
+    system "cmake", ".", *args
+    system "make", "install"
+  end
+end
