9a0a2363af33afa1b12d3e84cb63dc2cad4e21f1
Harald Fernengel <harald.fernengel@nokia.com>
2014-02-03 16:49:26 +0100

    WIP ktexteditor

---
+++ Library/Formula/ktexteditor.rb
@@ -0,0 +1,27 @@
+require "formula"
+
+class Ktexteditor < Formula
+  homepage "http://www.kde.org/"
+  url "http://download.kde.org/unstable/frameworks/4.95.0/ktexteditor-4.95.0.tar.xz"
+  sha1 ""
+
+  keg_only "Only required for building KDE applications"
+
+  head 'git://anongit.kde.org/ktexteditor.git'
+
+  depends_on "cmake" => :build
+  depends_on "extra-cmake-modules" => :build
+  depends_on "qt5" => :build
+  depends_on "karchive" => :build
+  depends_on "kjs" => :build
+  depends_on "ki18n" => :build
+  depends_on "kconfig" => :build
+
+  def install
+    args = std_cmake_args
+    args << "-DCMAKE_PREFIX_PATH=\"#{Formula.factory('qt5').opt_prefix};#{Formula.factory('extra-cmake-modules').opt_prefix}\""
+
+    system "cmake", ".", *args
+    system "make", "install"
+  end
+end
