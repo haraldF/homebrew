573e78d63b3a4eede1a4bf31a461040396ccb844
Harald Fernengel <harald.fernengel@nokia.com>
2014-02-04 15:00:46 +0100

    patch

--- Library/Formula/kconfigwidgets.rb
+++ Library/Formula/kconfigwidgets.rb
@@ -13,6 +13,11 @@ class Kconfigwidgets < Formula
   depends_on "kauth"
   depends_on "kcodecs"
 
+### hack - disable doctools als they are too fat
+  def patches
+    DATA
+  end
+
   def install
     args = std_cmake_args
     args << "-DCMAKE_PREFIX_PATH=\"#{Formula.factory('qt5').opt_prefix};#{Formula.factory('extra-cmake-modules').opt_prefix}\""
@@ -21,3 +26,25 @@ class Kconfigwidgets < Formula
     system "make", "install"
   end
 end
+
+__END__
+diff --git a/CMakeLists.txt b/CMakeLists.txt
+index b5b42fb..6343868 100644
+--- a/CMakeLists.txt
++++ b/CMakeLists.txt
+@@ -26,7 +26,7 @@ if(NOT kdelibs_SOURCE_DIR)
+   find_package(KF5CoreAddons ${KF5_VERSION} REQUIRED)
+   find_package(KF5Codecs ${KF5_VERSION} REQUIRED)
+   find_package(KF5Config ${KF5_VERSION} REQUIRED)
+-  find_package(KF5DocTools ${KF5_VERSION} REQUIRED)
++# find_package(KF5DocTools ${KF5_VERSION} REQUIRED)
+   find_package(KF5GuiAddons ${KF5_VERSION} REQUIRED)
+   find_package(KF5I18n ${KF5_VERSION} REQUIRED)
+   find_package(KF5WidgetsAddons ${KF5_VERSION} REQUIRED)
+diff --git a/docs/preparetips/CMakeLists.txt b/docs/preparetips/CMakeLists.txt
+index 1ab2dbe..c47dd8a 100644
+--- a/docs/preparetips/CMakeLists.txt
++++ b/docs/preparetips/CMakeLists.txt
+@@ -1 +1 @@
+-kdoctools_create_manpage(man-preparetips5.1.docbook 1 INSTALL_DESTINATION ${MAN_INSTALL_DIR})
++#kdoctools_create_manpage(man-preparetips5.1.docbook 1 INSTALL_DESTINATION ${MAN_INSTALL_DIR})
