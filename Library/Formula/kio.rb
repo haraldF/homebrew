817a285f444be732f60671c7ab2087c4dda9002d
Harald Fernengel <harald.fernengel@nokia.com>
2014-02-04 14:47:59 +0100

    more formula

---
+++ Library/Formula/kio.rb
@@ -0,0 +1,22 @@
+require "formula"
+
+class Kio < Formula
+  homepage "http://www.kde.org/"
+  url "http://download.kde.org/unstable/frameworks/4.95.0/kio-4.95.0.tar.xz"
+  sha1 ""
+
+  head 'git://anongit.kde.org/kio.git'
+
+  depends_on "cmake" => :build
+  depends_on "extra-cmake-modules" => :build
+  depends_on "qt5"
+  depends_on "kdbusaddons"
+
+  def install
+    args = std_cmake_args
+    args << "-DCMAKE_PREFIX_PATH=\"#{Formula.factory('qt5').opt_prefix};#{Formula.factory('extra-cmake-modules').opt_prefix}\""
+
+    system "cmake", ".", *args
+    system "make", "install"
+  end
+end
